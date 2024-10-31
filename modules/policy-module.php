7c30894371c601eb7f93365ce0c0bf1f66a6d11a
doulsi <marc.vanderli@gmail.com>
2024-10-31 14:51:04 +0100

    policy module v0.1

--- modules/policy-module.php
+++ modules/policy-module.php
@@ -3,11 +3,11 @@
 class PolicyModule {
     private $config;
     
-    public function __construct($config) {
+    public function __construct() {
         $this->loadConfigPolicy();
     }
 	private function loadConfigPolicy() {
-		$configPath = __DIR__ . '/../config/biodiversity-config.json';		
+		$configPath = __DIR__ . '/../config/policy-config.json';		
 		$this->config = json_decode(file_get_contents($configPath), true);
 		if (json_last_error() !== JSON_ERROR_NONE) {
 			throw new Exception('Failed to load policy game configuration');		
@@ -15,57 +15,35 @@ class PolicyModule {
 		error_log(sprintf('[Marco] loadConfigPolicy [%s]', print_r($this->config, true)));		
 	}
     
-    public function getStatusPolicy() {
+    public function getActions() {
         error_log(sprintf('[Marco] getStatusPolicy [%s]', print_r($this->config, true)));
+		return $this->config;
     }
         
     public function processAction($action, $subAction, $gameState) {
-        $actions = $this->getStatusPolicy();
-        if (!isset($actions[$action])) {
+        $actions = $this->getActions();
+        if (!isset($actions[$action][$subAction])) {
+			error_log(sprintf('[Marco] policy : subAction [%s] does not exist for module [%s]', print_r($subAction, true), print_r($actions, true)));
             return $gameState;
         }
+        error_log(sprintf('[Marco] policy processAction [%s]', print_r($actions[$action][$subAction], true)));
         
-        $actionConfig = $actions[$action];
-        if ($gameState['playerResources']['credits'] >= $actionConfig['cost']) {
+        $actionConfig = $actions[$action][$subAction];
+        if ($gameState['playerResources']['credits'] >= $actionConfig['cost']) 
+		{
             $gameState['playerResources']['credits'] -= $actionConfig['cost'];
             
-            $subActionConfig = $actionConfig['subActions'][$subAction] ?? $actionConfig['subActions']['carbon_tax'];
-            
-            // Base influence increase
-            $gameState['playerResources']['influence'] += $subActionConfig['policyEffect'];
-            
-            // Apply specific policy effects
-            switch ($subAction) {
-                case 'carbon_tax':
-                    $gameState['economy']['globalGDP'] *= (1 + $subActionConfig['economicImpact']);
-                    $gameState['metrics']['pollutionLevel'] *= (1 - $subActionConfig['emissionReduction']);
-                    break;
-                    
-                case 'plastic_ban':
-                    $gameState['metrics']['pollutionLevel'] *= (1 - $subActionConfig['pollutionReduction']);
-                    $gameState['metrics']['biodiversityIndex'] += $subActionConfig['biodiversityBoost'];
-                    break;
-                    
-                case 'emission_standards':
-                    $gameState['metrics']['pollutionLevel'] *= (1 - $subActionConfig['airQualityImprovement']);
-                    $gameState['economy']['circularEconomy'] += $subActionConfig['industryCompliance'];
-                    break;
-            }
+            $gameState['metrics']['pollutionIndex']       += $actionConfig['pollutionIndex'] + (0.001 * $gameState['metrics']['climateEducationIndex']);
+			$gameState['metrics']['economicImpactIndex']  += $actionConfig['economicImpactIndex'] + (0.001 * $gameState['metrics']['climateEducationIndex']);
+			$gameState['metrics']['circularEconomyIndex'] += $actionConfig['circularEconomyIndex'] + (0.001 * $gameState['metrics']['climateEducationIndex']);
         }
         
         return $gameState;
     }
     
     public function update($gameState) {
-        // Policy effectiveness decay over time
-        $gameState['playerResources']['influence'] *= 0.98;
         
-        // Influence benefits
-        if ($gameState['playerResources']['influence'] > 0) {
-            // Higher influence reduces pollution increase
-            $gameState['metrics']['pollutionLevel'] *= 
-                (1 - ($gameState['playerResources']['influence'] * 0.01));
-        }
+		
         
         return $gameState;
     }
